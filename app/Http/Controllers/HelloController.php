347e5add19b5257bf022e1015682d28098e64797
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-20 22:55:12 +0700

    Route Parameter

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,4 +10,7 @@ class HelloController extends Controller
     function showHello(){
         return '<h1>Hello Controller : SunvoDz </h1>';
     }
+    function showHelloName($name){
+        return '<h1>Hello Controller : '.$name.'</h1>';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('users', function () {
     return 'Sunvodz !';
 });
+
 Route::get('showname', function () {
     return 'Hello Sunvodz !';
 });
+
 Route::get('welcome/name','HelloController@showHello');
+
+Route::get('showname/{name}','HelloController@showHelloName');
